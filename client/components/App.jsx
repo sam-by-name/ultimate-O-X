15853ec64612dae771f6366579752082502055dd
sam-by-name <sam-sanders@hotmail.co.uk>
2018-07-31 12:36:16 +1200

    basic board begins

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import Board from './Board'
+
 class App extends React.Component {
   constructor (props) {
     super(props)
@@ -11,6 +13,7 @@ class App extends React.Component {
     return (
       <div>
         <h1>hi</h1>
+        <Board />
       </div>
     )
   }
--- client/components/Board.jsx
+++ client/components/Board.jsx
@@ -11,20 +11,23 @@ class Board extends React.Component {
     return (
       <div className='mainBoard'>
         <div className='TopRow'>
-          <div></div>
-          <div></div>
-          <div></div>
+          <div className='tL'>x</div>
+          <div className='tM'>x</div>
+          <div className='tR'>x</div>
         </div>
+        <div className='clear'></div>
         <div className='MidRow'>
-          <div></div>
-          <div></div>
-          <div></div>
+          <div className='mL'>x</div>
+          <div className='mM'>x</div>
+          <div className='mR'>x</div>
         </div>
+        <div className='clear'></div>
         <div className='BotRow'>
-          <div></div>
-          <div></div>
-          <div></div>
+          <div className='bL'>x</div>
+          <div className='bM'>x</div>
+          <div className='bR'>x</div>
         </div>
+        <div className='clear'></div>
       </div>
     )
   }
