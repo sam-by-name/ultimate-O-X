f55ac5922c46802cb9d094df07a32532d1535fcd
sam-by-name <sam-sanders@hotmail.co.uk>
2018-08-02 15:03:40 +1200

    minor edits to main/mini board

--- client/components/MainBoard.jsx
+++ client/components/MainBoard.jsx
@@ -26,21 +26,21 @@ class MainBoard extends Component {
     return (
       <div className='mainBoard'>
         <div className='topRow'>
-          <div className='fl'>   <MiniBoard onClick={this.handleClick} value={0} /></div>
-          <div className='fl tM'><MiniBoard onClick={this.handleClick} value={1} /></div>
-          <div className='fl'>   <MiniBoard onClick={this.handleClick} value={2} /></div>
+          <div className='fl'>   <MiniBoard handleClick={this.handleClick} value={0} /></div>
+          <div className='fl tM'><MiniBoard handleClick={this.handleClick} value={1} /></div>
+          <div className='fl'>   <MiniBoard handleClick={this.handleClick} value={2} /></div>
         </div>
         <div className='clear'></div>
         <div className='midRow'>
-          <div className='fl mL'><MiniBoard onClick={this.handleClick} value={3} /></div>
-          <div className='fl mM'><MiniBoard onClick={this.handleClick} value={4} /></div>
-          <div className='fl mR'><MiniBoard onClick={this.handleClick} value={5} /></div>
+          <div className='fl mL'><MiniBoard handleClick={this.handleClick} value={3} /></div>
+          <div className='fl mM'><MiniBoard handleClick={this.handleClick} value={4} /></div>
+          <div className='fl mR'><MiniBoard handleClick={this.handleClick} value={5} /></div>
         </div>
         <div className='clear'></div>
         <div className='botRow'>
-          <div className='fl'>   <MiniBoard onClick={this.handleClick} value={6} /></div>
-          <div className='fl bM'><MiniBoard onClick={this.handleClick} value={7} /></div>
-          <div className='fl'>   <MiniBoard onClick={this.handleClick} value={8} /></div>
+          <div className='fl'>   <MiniBoard handleClick={this.handleClick} value={6} /></div>
+          <div className='fl bM'><MiniBoard handleClick={this.handleClick} value={7} /></div>
+          <div className='fl'>   <MiniBoard handleClick={this.handleClick} value={8} /></div>
         </div>
         <div className='clear'></div>
       </div>
--- client/components/MiniBoard.jsx
+++ client/components/MiniBoard.jsx
@@ -4,21 +4,21 @@ const MiniBoard = (props) => {
   return (
     <div className='miniBoard'>
       <div className='topRow'>
-        <div onClick={this.props.onClick.handleClick} value={0} className='cell'></div>
-        <div onClick={this.props.onClick.handleClick} value={1} className='tM cell'></div>
-        <div onClick={this.props.onClick.handleClick} value={2} className='cell'></div>
+        <div onClick={props.handleClick} value={0} className='cell'></div>
+        <div onClick={props.handleClick} value={1} className='tM cell'></div>
+        <div onClick={props.handleClick} value={2} className='cell'></div>
       </div>
       <div className='clear'></div>
       <div className='midRow'>
-        <div onClick={this.props.onClick.handleClick} value={3} className='mL cell'></div>
-        <div onClick={this.props.onClick.handleClick} value={4} className='mM cell'></div>
-        <div onClick={this.props.onClick.handleClick} value={5} className='mR cell'></div>
+        <div onClick={props.handleClick} value={3} className='mL cell'></div>
+        <div onClick={props.handleClick} value={4} className='mM cell'></div>
+        <div onClick={props.handleClick} value={5} className='mR cell'></div>
       </div>
       <div className='clear'></div>
       <div className='botRow'>
-        <div onClick={this.props.onClick.handleClick} value={6} className='cell'></div>
-        <div onClick={this.props.onClick.handleClick} value={7} className='bM cell'></div>
-        <div onClick={this.props.onClick.handleClick} value={8} className='cell'></div>
+        <div onClick={props.handleClick} value={6} className='cell'></div>
+        <div onClick={props.handleClick} value={7} className='bM cell'></div>
+        <div onClick={props.handleClick} value={8} className='cell'></div>
       </div>
       <div className='clear'></div>
     </div>
