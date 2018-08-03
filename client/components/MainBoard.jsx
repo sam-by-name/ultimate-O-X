20c0879162711d1fc2f1ba0de0e3d66929478ef4
MichaelMitch <michael.j.mitchelson@gmail.com>
2018-08-03 14:29:37 +1200

    clean genBoard, start of mapping

--- client/components/MainBoard.jsx
+++ client/components/MainBoard.jsx
@@ -1,29 +1,29 @@
 import React, {Component} from 'react'
-import MiniBoard from './MiniBoard'
+import {gameArr} from '../../lib/gameArr'
 
 class MainBoard extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      gameArr: gameArr
+    }
+  }
   render () {
     let player = this.props.player
     return (
-      <div className='mainBoard'>
-        <div className='topRow'>
-          <div className='fl'>   <MiniBoard player={player} value={0} /></div>
-          <div className='fl tM'><MiniBoard player={player} value={1} /></div>
-          <div className='fl'>   <MiniBoard player={player} value={2} /></div>
+      <div>
+      {this.state.gameArr.map(miniBoard => {
+        return(
+        <div>
+          {miniboard.map(cell => {
+            return (
+              <div />
+            )
+          })}
         </div>
-        <div className='clear'></div>
-        <div className='midRow'>
-          <div className='fl mL'><MiniBoard player={player} value={3} /></div>
-          <div className='fl mM'><MiniBoard player={player} value={4} /></div>
-          <div className='fl mR'><MiniBoard player={player} value={5} /></div>
-        </div>
-        <div className='clear'></div>
-        <div className='botRow'>
-          <div className='fl'>   <MiniBoard player={player} value={6} /></div>
-          <div className='fl bM'><MiniBoard player={player} value={7} /></div>
-          <div className='fl'>   <MiniBoard player={player} value={8} /></div>
-        </div>
-        <div className='clear'></div>
+        )
+      })}
+
       </div>
     )
   }
