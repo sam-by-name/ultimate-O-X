5f9513e6a7b4e457d7708d87be4bd54e58d65659
sam-by-name <sam-sanders@hotmail.co.uk>
2018-08-09 17:31:29 +1200

    refactored scoreboard position and css

--- client/components/Title.jsx
+++ client/components/Title.jsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 const Title = () => {
   return (
     <div className='titleCont'>
-      <Link to='/'><h1 className='title' >Ultimate noughts and crosses</h1></Link>
+      <Link to='/'><h1 className='title' >Ultimate Naughts and Crosses</h1></Link>
     </div>
   )
 }
