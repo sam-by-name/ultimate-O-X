05607f6950aafc6fe92602314a74c7769756ec38
sam-by-name <sam-sanders@hotmail.co.uk>
2018-10-31 18:13:27 +1300

    findsPlayable tested for altering aiDb if true added as last argument

--- tests/client/ai/findsPlayable.test.js
+++ tests/client/ai/findsPlayable.test.js
@@ -1,14 +1,21 @@
 const {findsPlayable} = require('../../../lib/medAiV2/lib/getInfoV2')
 
 test('finds playable and owned moves', () => {
-  const expected = {aiOwns: '145', playable: '268', humanOwns: '037'}
-  const actual = findsPlayable(arr, 0, 'Player2', 'Player1')
+  // if true not added as last argument, aiDb is not marked out
+  const expected = {aiOwns: '145', playable: '0268', humanOwns: '37', aiDb: aiDb}
+  const actual = findsPlayable(arr, 0, 'Player2', 'Player1', aiDb, false)
+  expect(actual).toEqual(expected)
+})
+
+test('finds playable and owned moves, then marks out aiDb', () => {
+  const expected = {aiOwns: '145', playable: '0268', humanOwns: '37', aiDb: alteredAiDb}
+  const actual = findsPlayable(arr, 0, 'Player2', 'Player1', aiDb, true)
   expect(actual).toEqual(expected)
 })
 
 const arr = [
   [
-    {takenBy: 'Player1'},
+    {takenBy: ''},
     {takenBy: 'Player2'},
     {takenBy: ''},
     {takenBy: 'Player1'},
@@ -17,5 +24,33 @@ const arr = [
     {takenBy: ''},
     {takenBy: 'Player1'},
     {takenBy: ''}
+  ]
+]
+
+const aiDb = [
+  [
+    {aiOwns: false, humanOwns: false, playable: false, sends2Same: false},
+    {aiOwns: false, humanOwns: false, playable: false, sends2Same: false},
+    {aiOwns: false, humanOwns: false, playable: false, sends2Same: false},
+    {aiOwns: false, humanOwns: false, playable: false, sends2Same: false},
+    {aiOwns: false, humanOwns: false, playable: false, sends2Same: false},
+    {aiOwns: false, humanOwns: false, playable: false, sends2Same: false},
+    {aiOwns: false, humanOwns: false, playable: false, sends2Same: false},
+    {aiOwns: false, humanOwns: false, playable: false, sends2Same: false},
+    {aiOwns: false, humanOwns: false, playable: false, sends2Same: false}
+  ]
+]
+
+const alteredAiDb = [
+  [
+    {aiOwns: false, humanOwns: false, playable: true, sends2Same: true},
+    {aiOwns: true, humanOwns: false, playable: false, sends2Same: false},
+    {aiOwns: false, humanOwns: false, playable: true, sends2Same: false},
+    {aiOwns: false, humanOwns: true, playable: false, sends2Same: false},
+    {aiOwns: true, humanOwns: false, playable: false, sends2Same: false},
+    {aiOwns: true, humanOwns: false, playable: false, sends2Same: false},
+    {aiOwns: false, humanOwns: false, playable: true, sends2Same: false},
+    {aiOwns: false, humanOwns: true, playable: false, sends2Same: false},
+    {aiOwns: false, humanOwns: false, playable: true, sends2Same: false}
   ]
 ]
