a74fab98570fd93ddbc9358fa58a18e3b9fecd51
sam-by-name <sam-sanders@hotmail.co.uk>
2018-10-31 19:12:46 +1300

    test for findsMiniGame and marks out aiDb passing

--- tests/client/ai/findsMiniGame.test.js
+++ tests/client/ai/findsMiniGame.test.js
@@ -1,5 +1,6 @@
-const {findsMiniGame} = require('../../../lib/medAiV2/lib/getInfoV2')
-const {aiDb} = require('./lib/aiDbMock')
+const {findsMiniGame, findsPlayable} = require('../../../lib/medAiV2/lib/getInfoV2')
+// const {aiDb} = require('./lib/aiDbMock')
+// const {alteredAiDb} = require('./lib/alteredAiDbMock')
 const {arr} = require('./lib/arrMock')
 
 test('finds and returns an array of playable miniGames', () => {
@@ -7,3 +8,34 @@ test('finds and returns an array of playable miniGames', () => {
   const actual = findsMiniGame(arr, [], aiDb)
   expect(actual).toEqual(expected)
 })
+
+test('finds playable miniGames and marks out aiDb', () => {
+  const expected = alteredAiDb
+  findsMiniGame(arr, [], aiDb)
+  const actual = aiDb
+  expect(actual).toEqual(expected)
+})
+
+const aiDb = [
+  [{miniPlayable: false}],
+  [{miniPlayable: false}],
+  [{miniPlayable: false}],
+  [{miniPlayable: false}],
+  [{miniPlayable: false}],
+  [{miniPlayable: false}],
+  [{miniPlayable: false}],
+  [{miniPlayable: false}],
+  [{miniPlayable: false}]
+]
+
+const alteredAiDb = [
+  [{miniPlayable: false}],
+  [{miniPlayable: false}],
+  [{miniPlayable: true}],
+  [{miniPlayable: false}],
+  [{miniPlayable: true}],
+  [{miniPlayable: false}],
+  [{miniPlayable: false}],
+  [{miniPlayable: false}],
+  [{miniPlayable: false}]
+]
