2b78c54513ee663e580ce4f4e18626fe9f43b6a5
sam-by-name <sam-sanders@hotmail.co.uk>
2018-11-01 21:43:11 +1300

    findsNewLine tested for marking of big line

--- tests/client/ai/findsNewLine.test.js
+++ tests/client/ai/findsNewLine.test.js
@@ -7,6 +7,13 @@ test('findsNewLine, sets aiDb newLine flags', () => {
   expect(actual).toEqual(expected)
 })
 
+test('findsNewLine, sets aiDb newLine flags', () => {
+  const expected = bigAlteredAiDb
+  findsNewLine('25678', 0, bigAiDb, false)
+  const actual = bigAiDb
+  expect(actual).toEqual(expected)
+})
+
 const aiDb = [
   [
     {startsNew: false, startsMostNew: false},
@@ -32,5 +39,209 @@ const alteredAiDb = [
     {startsNew: true, startsMostNew: false},
     {startsNew: true, startsMostNew: false},
     {startsNew: false, startsMostNew: true}
+  ]
+]
+
+const bigAiDb = [
+  [
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false}
+  ],
+  [
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false}
+  ],
+  [
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false}
+  ],
+  [
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false}
+  ],
+  [
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false}
+  ],
+  [
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false}
+  ],
+  [
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false}
+  ],
+  [
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false}
+  ],
+  [
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false}
+  ]
+]
+
+const bigAlteredAiDb = [
+  [
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false}
+  ],
+  [
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false}
+  ],
+  [
+    {bigStartsNew: true, bigStartsMostNew: false},
+    {bigStartsNew: true, bigStartsMostNew: false},
+    {bigStartsNew: true, bigStartsMostNew: false},
+    {bigStartsNew: true, bigStartsMostNew: false},
+    {bigStartsNew: true, bigStartsMostNew: false},
+    {bigStartsNew: true, bigStartsMostNew: false},
+    {bigStartsNew: true, bigStartsMostNew: false},
+    {bigStartsNew: true, bigStartsMostNew: false},
+    {bigStartsNew: true, bigStartsMostNew: false}
+  ],
+  [
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false}
+  ],
+  [
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false},
+    {bigStartsNew: false, bigStartsMostNew: false}
+  ],
+  [
+    {bigStartsNew: true, bigStartsMostNew: false},
+    {bigStartsNew: true, bigStartsMostNew: false},
+    {bigStartsNew: true, bigStartsMostNew: false},
+    {bigStartsNew: true, bigStartsMostNew: false},
+    {bigStartsNew: true, bigStartsMostNew: false},
+    {bigStartsNew: true, bigStartsMostNew: false},
+    {bigStartsNew: true, bigStartsMostNew: false},
+    {bigStartsNew: true, bigStartsMostNew: false},
+    {bigStartsNew: true, bigStartsMostNew: false}
+  ],
+  [
+    {bigStartsNew: true, bigStartsMostNew: false},
+    {bigStartsNew: true, bigStartsMostNew: false},
+    {bigStartsNew: true, bigStartsMostNew: false},
+    {bigStartsNew: true, bigStartsMostNew: false},
+    {bigStartsNew: true, bigStartsMostNew: false},
+    {bigStartsNew: true, bigStartsMostNew: false},
+    {bigStartsNew: true, bigStartsMostNew: false},
+    {bigStartsNew: true, bigStartsMostNew: false},
+    {bigStartsNew: true, bigStartsMostNew: false}
+  ],
+  [
+    {bigStartsNew: true, bigStartsMostNew: false},
+    {bigStartsNew: true, bigStartsMostNew: false},
+    {bigStartsNew: true, bigStartsMostNew: false},
+    {bigStartsNew: true, bigStartsMostNew: false},
+    {bigStartsNew: true, bigStartsMostNew: false},
+    {bigStartsNew: true, bigStartsMostNew: false},
+    {bigStartsNew: true, bigStartsMostNew: false},
+    {bigStartsNew: true, bigStartsMostNew: false},
+    {bigStartsNew: true, bigStartsMostNew: false}
+  ],
+  [
+    {bigStartsNew: false, bigStartsMostNew: true},
+    {bigStartsNew: false, bigStartsMostNew: true},
+    {bigStartsNew: false, bigStartsMostNew: true},
+    {bigStartsNew: false, bigStartsMostNew: true},
+    {bigStartsNew: false, bigStartsMostNew: true},
+    {bigStartsNew: false, bigStartsMostNew: true},
+    {bigStartsNew: false, bigStartsMostNew: true},
+    {bigStartsNew: false, bigStartsMostNew: true},
+    {bigStartsNew: false, bigStartsMostNew: true}
   ]
 ]
