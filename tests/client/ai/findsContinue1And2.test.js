58c20d0c937ed74266a17e6240916df3baa38576
sam-by-name <sam-sanders@hotmail.co.uk>
2018-11-01 17:21:07 +1300

    findsContinue1And2 tested for marking out small game and big game, tests passing

--- tests/client/ai/findsContinue1And2.test.js
+++ tests/client/ai/findsContinue1And2.test.js
@@ -7,6 +7,13 @@ test('takes counted, assigns boolean to aiDb according to count', () => {
   expect(actual).toEqual(expected)
 })
 
+test('takes counted, assigns true to continue1 || continue2 accordingly', () => {
+  const expected = bigAlteredAiDb
+  findsContinue1And2(counted, 0, bigAiDb, false)
+  const actual = bigAiDb
+  expect(actual).toEqual(expected)
+})
+
 const counted = [
   {value: 4, count: 2},
   {value: 2, count: 2},
@@ -18,28 +25,232 @@ const counted = [
 
 const aiDb = [
   [
-    {continue1: false, continue2: false, bigContinue1: false, bigContinue2: false},
-    {continue1: false, continue2: false, bigContinue1: false, bigContinue2: false},
-    {continue1: false, continue2: false, bigContinue1: false, bigContinue2: false},
-    {continue1: false, continue2: false, bigContinue1: false, bigContinue2: false},
-    {continue1: false, continue2: false, bigContinue1: false, bigContinue2: false},
-    {continue1: false, continue2: false, bigContinue1: false, bigContinue2: false},
-    {continue1: false, continue2: false, bigContinue1: false, bigContinue2: false},
-    {continue1: false, continue2: false, bigContinue1: false, bigContinue2: false},
-    {continue1: false, continue2: false, bigContinue1: false, bigContinue2: false}
+    {continue1: false, continue2: false},
+    {continue1: false, continue2: false},
+    {continue1: false, continue2: false},
+    {continue1: false, continue2: false},
+    {continue1: false, continue2: false},
+    {continue1: false, continue2: false},
+    {continue1: false, continue2: false},
+    {continue1: false, continue2: false},
+    {continue1: false, continue2: false}
   ]
 ]
 
 const alteredAiDb = [
   [
-    {continue1: true, continue2: false, bigContinue1: false, bigContinue2: false},
-    {continue1: false, continue2: false, bigContinue1: false, bigContinue2: false},
-    {continue1: false, continue2: true, bigContinue1: false, bigContinue2: false},
-    {continue1: true, continue2: false, bigContinue1: false, bigContinue2: false},
-    {continue1: false, continue2: true, bigContinue1: false, bigContinue2: false},
-    {continue1: false, continue2: false, bigContinue1: false, bigContinue2: false},
-    {continue1: false, continue2: false, bigContinue1: false, bigContinue2: false},
-    {continue1: true, continue2: false, bigContinue1: false, bigContinue2: false},
-    {continue1: true, continue2: false, bigContinue1: false, bigContinue2: false}
+    {continue1: true, continue2: false},
+    {continue1: false, continue2: false},
+    {continue1: false, continue2: true},
+    {continue1: true, continue2: false},
+    {continue1: false, continue2: true},
+    {continue1: false, continue2: false},
+    {continue1: false, continue2: false},
+    {continue1: true, continue2: false},
+    {continue1: true, continue2: false}
+  ]
+]
+
+const bigAiDb = [
+  [
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false}
+  ],
+  [
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false}
+  ],
+  [
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false}
+  ],
+  [
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false}
+  ],
+  [
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false}
+  ],
+  [
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false}
+  ],
+  [
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false}
+  ],
+  [
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false}
+  ],
+  [
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false}
+  ]
+]
+
+const bigAlteredAiDb = [
+  [
+    {bigContinue1: true, bigContinue2: false},
+    {bigContinue1: true, bigContinue2: false},
+    {bigContinue1: true, bigContinue2: false},
+    {bigContinue1: true, bigContinue2: false},
+    {bigContinue1: true, bigContinue2: false},
+    {bigContinue1: true, bigContinue2: false},
+    {bigContinue1: true, bigContinue2: false},
+    {bigContinue1: true, bigContinue2: false},
+    {bigContinue1: true, bigContinue2: false}
+  ],
+  [
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false}
+  ],
+  [
+    {bigContinue1: false, bigContinue2: true},
+    {bigContinue1: false, bigContinue2: true},
+    {bigContinue1: false, bigContinue2: true},
+    {bigContinue1: false, bigContinue2: true},
+    {bigContinue1: false, bigContinue2: true},
+    {bigContinue1: false, bigContinue2: true},
+    {bigContinue1: false, bigContinue2: true},
+    {bigContinue1: false, bigContinue2: true},
+    {bigContinue1: false, bigContinue2: true}
+  ],
+  [
+    {bigContinue1: true, bigContinue2: false},
+    {bigContinue1: true, bigContinue2: false},
+    {bigContinue1: true, bigContinue2: false},
+    {bigContinue1: true, bigContinue2: false},
+    {bigContinue1: true, bigContinue2: false},
+    {bigContinue1: true, bigContinue2: false},
+    {bigContinue1: true, bigContinue2: false},
+    {bigContinue1: true, bigContinue2: false},
+    {bigContinue1: true, bigContinue2: false}
+  ],
+  [
+    {bigContinue1: false, bigContinue2: true},
+    {bigContinue1: false, bigContinue2: true},
+    {bigContinue1: false, bigContinue2: true},
+    {bigContinue1: false, bigContinue2: true},
+    {bigContinue1: false, bigContinue2: true},
+    {bigContinue1: false, bigContinue2: true},
+    {bigContinue1: false, bigContinue2: true},
+    {bigContinue1: false, bigContinue2: true},
+    {bigContinue1: false, bigContinue2: true}
+  ],
+  [
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false}
+  ],
+  [
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false},
+    {bigContinue1: false, bigContinue2: false}
+  ],
+  [
+    {bigContinue1: true, bigContinue2: false},
+    {bigContinue1: true, bigContinue2: false},
+    {bigContinue1: true, bigContinue2: false},
+    {bigContinue1: true, bigContinue2: false},
+    {bigContinue1: true, bigContinue2: false},
+    {bigContinue1: true, bigContinue2: false},
+    {bigContinue1: true, bigContinue2: false},
+    {bigContinue1: true, bigContinue2: false},
+    {bigContinue1: true, bigContinue2: false}
+  ],
+  [
+    {bigContinue1: true, bigContinue2: false},
+    {bigContinue1: true, bigContinue2: false},
+    {bigContinue1: true, bigContinue2: false},
+    {bigContinue1: true, bigContinue2: false},
+    {bigContinue1: true, bigContinue2: false},
+    {bigContinue1: true, bigContinue2: false},
+    {bigContinue1: true, bigContinue2: false},
+    {bigContinue1: true, bigContinue2: false},
+    {bigContinue1: true, bigContinue2: false}
   ]
 ]
